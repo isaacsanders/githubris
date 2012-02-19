9c13388efd8ce3e3f342121cd1be9865ea8a7d0b
Isaac Sanders <isaacbfsanders@gmail.com>
2012-02-18 20:01:04 -0500

    Added the modules for the API

--- lib/githubris.rb
+++ lib/githubris.rb
@@ -1,5 +1,28 @@
 require "githubris/version"
 
 module Githubris
-  # Your code goes here...
+  autoload 'Blob', 'githubris/blob'
+  autoload 'Comment', 'githubris/comment'
+  autoload 'Commit', 'githubris/commit'
+  autoload 'Event', 'githubris/event'
+  autoload 'Gist', 'githubris/gist'
+
+  autoload 'Issue'            , 'githubris/issue'
+  autoload 'Issue::Label'     , 'githubris/issue/label'
+  autoload 'Issue::Milestone' , 'githubris/issue/milestone'
+
+  autoload 'Organization'       , 'githubris/organization'
+  autoload 'Organization::Team' , 'githubris/organization/team'
+
+  autoload 'PullRequest', 'githubris/pull_request'
+  autoload 'Reference', 'githubris/reference'
+
+  autoload 'Repository'           , 'githubris/repository'
+  autoload 'Repository::Download' , 'githubris/repository/download'
+  autoload 'Repository::Fork'     , 'githubris/repository/fork'
+  autoload 'Repository::Hook'     , 'githubris/repository/hook'
+
+  autoload 'Tag', 'githubris/tag'
+  autoload 'Tree', 'githubris/tree'
+  autoload 'User', 'githubris/user'
 end
