ce48c6e8a4306f1ad5d61d12825da507c66c5d82
Isaac Sanders <isaacbfsanders@gmail.com>
2012-02-20 00:18:33 -0500

    Added api specs

--- lib/githubris/api.rb
+++ lib/githubris/api.rb
@@ -1,4 +1,16 @@
 module Githubris
   class API
+    def self.call(*args)
+      options = args.inject(Githubris::Config) {|config, key| config[key]}
+      Githubris::API.new(options).resolve
+    end
+
+    def initialize options
+    end
+
+    def resolve
+      Net::HTTP.get('api.github.com', '/')
+      {}
+    end
   end
 end
--- spec/githubris/api_spec.rb
+++ spec/githubris/api_spec.rb
@@ -1,5 +1,34 @@
 require 'spec_helper'
 
 describe Githubris::API do
-  describe
+  describe 'making calls' do
+    subject { Githubris::API }
+
+    it 'takes multiple arguments' do
+      Githubris::Config.stub(:[] => {:bar => {:baz => nil}})
+      lambda {
+        subject.call(:foo)
+        subject.call(:foo, :bar)
+        subject.call(:foo, :bar, :baz)
+      }.should_not raise_error
+    end
+
+    it 'uses the config' do
+      Githubris::Config.stub(:[])
+      subject.call(:foo)
+      Githubris::Config.should have_received(:[]).with(:foo)
+    end
+
+    it 'creates a new api call instance' do
+      Githubris::Config.stub(:[] => {})
+      subject.call(:foo).should be_instance_of Hash
+    end
+  end
+
+  describe 'resolving calls' do
+    it 'calls github' do
+      Githubris::API.new({method: 'post', uri: '/foo/bar'}).resolve
+      FakeWeb.last_request.should_not be_nil
+    end
+  end
 end
