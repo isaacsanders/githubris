2e232c572fe057b425dcc46c480198666b799723
Isaac Sanders <isaacbfsanders@gmail.com>
2012-02-27 19:52:30 -0500

    Attempt to make ci tasks work

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,7 @@
 require 'rspec-spies'
 require 'fakeweb'
-require 'simplecov'
 require 'githubris'
 
 require 'support/fixtures'
 require 'support/fakeweb_responses'
 require 'support/custom_matchers'
-
-SimpleCov.start do
-  add_filter '/spec/'
-  add_filter '/features/'
-  add_filter '/pkg/'
-  add_filter '/coverage/'
-end
