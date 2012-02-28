a57266376fcd35007918927de8b66155c6e8b35f
Isaac Sanders <isaacbfsanders@gmail.com>
2012-02-27 19:16:03 -0500

    Added simplecov for code coverage

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ group :development, :test do
   gem 'rspec', '~> 2.8.0'
   gem 'rspec-spies'
   gem 'fakeweb'
+  gem 'simplecov'
 end
 
 gem 'rake'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,15 @@
 require 'rspec-spies'
 require 'fakeweb'
+require 'simplecov'
 require 'githubris'
 
 require 'support/fixtures'
 require 'support/fakeweb_responses'
 require 'support/custom_matchers'
+
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/features/'
+  add_filter '/pkg/'
+  add_filter '/coverage/'
+end
