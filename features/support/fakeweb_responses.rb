544f8b1505010dc1a879dabf313c634f2bdf4005
Isaac Sanders <isaacbfsanders@gmail.com>
2012-02-29 16:56:09 -0500

    Added fakeweb to features

---
+++ features/support/fakeweb_responses.rb
@@ -0,0 +1,6 @@
+require 'fakeweb'
+
+FakeWeb.allow_net_connect = false
+FakeWeb.register_uri(:get, 'https://api.github.com/', :body => '{}')
+FakeWeb.register_uri(:get, /gists\/public(?:[^?]*\?page=(\d+))?/,
+                     :body => File.open("support/public_gists_page_#{$1 || 1}.json", 'r') {|f| f.read })
