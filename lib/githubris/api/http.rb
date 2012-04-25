8a84185249b7af2f3fae9c98d0e9e42a6fbf0134
Isaac Sanders <isanders@groupon.com>
2012-04-25 18:19:19 -0500

    Made the HTTP interface more obvious

--- lib/githubris/api/http.rb
+++ lib/githubris/api/http.rb
@@ -3,19 +3,23 @@ require 'httparty'
 module Githubris::API::HTTP
   private
 
-  def self.define_httparty_method(*http_methods)
-    http_methods.each do |http_method|
-      module_eval http_method_string(http_method)
-    end
+  def _get(path=@target.to_s, options=@options)
+    HTTParty.get(path, options)
   end
 
-  def self.http_method_string(http_method)
-    """
-    def _#{http_method}(path=@target.to_s, options=@options)
-      HTTParty.#{http_method}(path, options)
-    end
-    """
+  def _post(path=@target.to_s, options=@options)
+    HTTParty.get(path, options)
   end
 
-  define_httparty_method :get, :post, :patch, :put, :delete
+  def _put(path=@target.to_s, options=@options)
+    HTTParty.get(path, options)
+  end
+
+  def _patch(path=@target.to_s, options=@options)
+    HTTParty.get(path, options)
+  end
+
+  def _delete(path=@target.to_s, options=@options)
+    HTTParty.get(path, options)
+  end
 end
