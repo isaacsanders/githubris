10ca996eb894acc6044385bc9eb23ade186ff2fd
Isaac Sanders <isanders@groupon.com>
2012-04-26 08:36:19 -0500

    Added the MaxNumberOfLoginAttemptExceeded error class

--- lib/githubris/error.rb
+++ lib/githubris/error.rb
@@ -3,3 +3,4 @@ class Githubris::Error::RequiresAuthentication < Githubris::Error; end
 class Githubris::Error::BadCredentials < Githubris::Error; end
 class Githubris::Error::NotFound < Githubris::Error; end
 class Githubris::Error::InvalidAttributes < Githubris::Error; end
+class Githubris::Error::MaxNumberOfLoginAttemptExceeded < Githubris::Error; end
