07166422161a7b2f6699a4b1929b1d801b343eab
Isaac Sanders <isanders@groupon.com>
2012-04-26 09:11:43 -0500

    Fixed issues with the HTTP refactoring

--- lib/githubris/api/http.rb
+++ lib/githubris/api/http.rb
@@ -8,18 +8,18 @@ module Githubris::API::HTTP
   end
 
   def _post(path=@target.to_s, options=@options)
-    HTTParty.get(path, options)
+    HTTParty.post(path, options)
   end
 
   def _put(path=@target.to_s, options=@options)
-    HTTParty.get(path, options)
+    HTTParty.put(path, options)
   end
 
   def _patch(path=@target.to_s, options=@options)
-    HTTParty.get(path, options)
+    HTTParty.patch(path, options)
   end
 
   def _delete(path=@target.to_s, options=@options)
-    HTTParty.get(path, options)
+    HTTParty.delete(path, options)
   end
 end
