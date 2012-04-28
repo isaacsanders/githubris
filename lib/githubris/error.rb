043fdc409ad5c198849d292978a9cfe2ff274609
Isaac Sanders <isanders@groupon.com>
2012-04-28 13:17:20 -0500

    Added the ServerError class

--- lib/githubris/error.rb
+++ lib/githubris/error.rb
@@ -4,3 +4,4 @@ class Githubris::Error::BadCredentials < Githubris::Error; end
 class Githubris::Error::NotFound < Githubris::Error; end
 class Githubris::Error::InvalidAttributes < Githubris::Error; end
 class Githubris::Error::MaxNumberOfLoginAttemptExceeded < Githubris::Error; end
+class Githubris::Error::ServerError < Githubris::Error; end
