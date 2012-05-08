2c1008e1799af4ed55e4c0f8a1c4231ceb264245
Isaac Sanders <isanders@groupon.com>
2012-05-08 15:06:54 -0500

    Added ValidationError

--- lib/githubris/error.rb
+++ lib/githubris/error.rb
@@ -5,3 +5,4 @@ class Githubris::Error::NotFound < Githubris::Error; end
 class Githubris::Error::InvalidAttributes < Githubris::Error; end
 class Githubris::Error::MaxNumberOfLoginAttemptExceeded < Githubris::Error; end
 class Githubris::Error::ServerError < Githubris::Error; end
+class Githubris::Error::ValidationFailed < Githubris::Error; end
